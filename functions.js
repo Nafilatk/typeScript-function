cc8fe130b3f87a421a7c186a2c22a5d9aa726e0e
nafila <nafilatk7.com>
2025-10-13 12:18:00 +0530

    repush

--- functions.js
+++ functions.js
@@ -1,4 +1,28 @@
 function add(a, b) {
     return a + b;
 }
-var result = add(10, 5);
+console.log(add(2, 4));
+// function without return type
+function greet(a) {
+    console.log("hello", a);
+}
+//  function with default parameter
+function power(base, exponent) {
+    if (exponent === void 0) { exponent = 2; }
+    return base * exponent;
+}
+console.log(power(3));
+function powers(base, exponent) {
+    if (exponent === void 0) { exponent = 2; }
+    return Math.pow(base, exponent);
+}
+console.log(powers(3));
+function powerr(base, exponent) {
+    return base + exponent;
+}
+console.log(powerr(3, 5));
+function powerss(base, exponent) {
+    if (exponent === void 0) { exponent = "nafila"; }
+    return base + exponent;
+}
+console.log(powerss('hello'));
--- functions.ts
+++ functions.ts
@@ -1,5 +1,37 @@
 function add(a: number, b: number): number {
   return a + b;
 }
+ console.log(add(2,4));
 
-let result = add(10, 5);
+
+
+// function without return type
+
+
+function greet(a:string):void{
+  console.log("hello",a);
+}
+
+
+//  function with default parameter
+
+function power(base:number,exponent :number = 2):number{
+  return base * exponent
+}
+console.log(power(3));
+
+function powers(base:number,exponent :number = 2):number{
+  return base ** exponent
+}
+console.log(powers(3));
+
+function powerr(base:number,exponent :number ):number{
+  return base + exponent
+}
+console.log(powerr(3,5));
+
+
+function powerss(base:string,exponent :string = "nafila"):string{
+  return base + exponent
+}
+console.log(powerss('hello'));
